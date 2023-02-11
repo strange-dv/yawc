afd1f606f133f67f17f4cbcfab33ad872dba480b
strange-dv <andrii.dovzhenko.dv@gmail.com>
2023-02-12 00:33:15 +0200

    changing the second provider

--- src/providers/visualcrossing.rs
+++ src/providers/visualcrossing.rs
@@ -1,20 +1,23 @@
 use crate::providers::provider::Provider;
 use crate::providers::weather::Weather;
-use chrono::NaiveDate;
+use chrono::{DateTime, NaiveDate};
 
 /// Retrieves information about weather using OpenWeather API
-pub struct OpenWeather {
+pub struct AerisWeather {
     api_key: String,
     api_base_url: String,
 }
 
-impl OpenWeather {
-    pub fn new(api_key: String, api_base_url: String) -> OpenWeather {
-        OpenWeather { api_key, api_base_url }
+impl AerisWeather {
+    pub fn new(api_key: String, api_base_url: String) -> AerisWeather {
+        AerisWeather {
+            api_key,
+            api_base_url,
+        }
     }
 }
 
-impl Provider for OpenWeather {
+impl Provider for AerisWeather {
     fn get_weather(&self, address: String, date: NaiveDate) -> std::io::Result<Weather> {
         Ok(Weather::new(String::from("<weather>"), address, date))
     }
